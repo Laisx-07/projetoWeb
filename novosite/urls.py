7e92e0d5d7396d918ea453f98dc057339bb7ee69
Laís Alves da Silva <2025107290015@aluno.ifro.edu.br>
2026-05-21 22:05:45 -0400

    algumas alteracoes da aula 6

--- novosite/urls.py
+++ novosite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include  # Make sure 'include' is imported
+from django.urls import path, include
+
+from usuarios import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios.urls')), # This links your app's URLs
+    path('usuarios/', include('usuarios.urls')), 
 ]
